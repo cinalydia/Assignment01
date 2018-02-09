20842a6e8c97fbcbd4e9d3706e2fbef4a6424257
cinalydia <cinalydia@gmail.com>
2018-02-08 20:41:38 -0800

    Assignment01_Complete
    Assignment 1 is complete.

---
+++ js/script.js
@@ -0,0 +1,143 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*var someMonth;
+function theMonth();
+currentMonth
+var summerMonth;
+myLibraryAFunction*/
+
+//STEP2
+/*10
+"No!"
+false
+null*/
+
+//STEP3
+/*var superMath = 10 + 1;
+var superEverything = superMath * 3;*/
+
+//STEP4
+/*var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var iZipCode;
+var iYourAge;
+var strReferralSource;
+var strMayWeContactYou;*/
+
+//STEP5 
+/*var strFirstName;
+var strLastName;
+strFirstName = "Tina";
+strLastName = "Lydia";
+
+var strCity = "San Diego";
+var iZipCode = 92182;
+
+var iYourAge = "Twenty Something", strState = "California";*/
+
+//STEP6
+/*var adding = 1 + 4;
+var name = "Tina";
+window.console.log(name + " would like to be " + adding + " years of age.");*/
+/*var asking = false;
+var name = "Tina";
+window.console.log("I heard that " + name + " is eating pizza. Is that true?" + " No. It's " + asking + ".");*/
+/*var x = 500 + 200;
+var y = true;
+window.console.log("500 + 200 is " + x + "." + " Yes, that's " + y + ", my friend.");*/
+
+//STEP7
+/*var name = "Sara";
+window.console.log("Her name is " + not + " "+ name + ".");
+var not = "not";*/
+
+//STEP8
+/*No.
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);*/
+
+//STEP9
+/*var x = null;
+window.console.log(x);
+var none;
+window.console.log(none);*/
+
+//STEP10
+/*window.console.log(typeof "Hello");
+window.console.log(typeof 10);
+window.console.log(typeof true);
+window.console.log(typeof {age:19});
+window.console.log(typeof helloThere);*/
+
+//STEP11
+/*window.alert("Hello" + " Tina" + ", welcome to the JavaScript class!");*/
+
+//STEP12
+/*var name = "Tina";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+//STEP13
+/*var x = "Javascript";
+window.alert("Hello " + name + ", welcome to the " + x + " class!");*/
+
+//STEP14
+/*var x = "Javascript";
+window.alert("Hello " + name +"." + "\n" + "Welcome to the " + x + " class!");*/
+
+//STEP15
+/*var name = window.prompt("What is your name?");
+var x = "Javascript";
+window.alert("Hello " + name +"." + "\n" + "Welcome to the " + x + " class!");*/
+
+//STEP16
+/*var name = window.prompt("What is your name?");
+var x = window.prompt("What class are you taking?");
+window.alert("Hello " + name +"." + "\n" + "Welcome to the " + x + " class!");*/
+
+//STEP17
+/*var x = 10;
+var y = 20;
+window.console.log(x + y);*/
+
+//STEP18
+/*var x = 20;
+var y = x += 20;
+window.console.log(y);*/
+
+//STEP19
+/*var x = 20;
+var y = x *= 5;
+window.console.log(y);*/
+
+//STEP20
+/*var x = 20 % 3;
+var y = x /= 1;
+window.console.log(y);*/
+
+//STEP21
+/*var x = 10;
+var y = 25;
+window.console.log(x < y);
+window.console.log(x < y != false);*/
+
+//STEP22
+/*var x = 22 - 50;
+var y = x + 1;
+window.console.log(x + y == 35);
+window.console.log(x + y == 35 && 100);*/
+
+//STEP23
+/*var widget = new Object();
+window.console.log(typeof widget);*/
+
+//STEP24
+/*var widget = new Object();
+window.console.log(widget instanceof Object);*/
+
+//STEP25
+/*var widget = new Object();
+window.console.log(!(widget instanceof Object));*/
